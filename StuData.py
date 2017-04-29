3ecad95a565bef29b8f1ff0cc25c0ee0cd784ec5
Priya Singh <priyaSin@users.noreply.github.com>
2017-04-29 21:15:00 +0530

    StuData.py file changed

--- StuData.py
+++ StuData.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
 import psycopg2
 conn = psycopg2.connect(database="postgres", user="postgres", password="postgres")
 cursor = conn.cursor()
+def add_stu():
+	os.system("Add_Stu.py")
+def fetch_stu():
+	os.system("Fetch_Stu.py")
 def main(args):
     option=raw_input ("Select from the options below: \n 1.Adding Student Data \n 2. Fetching Student Record")
 	if option=1:
